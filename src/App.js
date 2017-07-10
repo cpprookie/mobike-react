c95bdc86b43301fbdf2b81b12bbb0fb910d85953
cpprookie <oldchen159@163.com>
2017-07-10 20:06:27 +0800

    deeper understanding for react-router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
 import Footer from './components/Footer'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/global" component={Home} />
-          <Footer />
-        </div>
-      </Router>
+      <div>
+        <Route exact path="/global" component={Home} />
+        <Footer />
+      </div>
     )
   }
 }

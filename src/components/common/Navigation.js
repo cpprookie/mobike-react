c14cd979f3b409007017b32f1d39bed9fd94e4a7
cpprookie <oldchen159@163.com>
2017-07-12 20:18:15 +0800

    finish mobile navigation

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -19,6 +19,26 @@ class Navigation extends Component {
             <a href="/request-form" className="navigation-item">Request Mobike</a>
             <a href="/download" className="navigation-item navigation-download">Download App</a>
           </div>
+          <input type="checkbox" id="navigation-trigger" className="navigation-trigger" />
+          <label className="navigation-humburger" htmlFor="navigation-trigger">
+            <div className="navigation-humburger-icon"></div>
+          </label>
+          <div className="navigation-mobile">
+            <label className="navigation-close" htmlFor="navigation-trigger">
+              <div className="navigation-close-icon"></div>
+            </label>
+            <div className="navigation-mobile-items">
+              <label htmlFor="navigation-trigger">
+                <img src="https://mobike.com/global/public/footer_logo_mobike_navigation.jpg" className="navigation-mobile-logo" alt="mobile-logo" />
+              </label>
+              <a className="navigation-mobile-item" href="/">HOME</a>
+              <a className="navigation-mobile-item" href="/about">ABOUT</a>
+              <a className="navigation-mobile-item" href="blog">BLOG</a>
+              <a className="navigation-mobile-item" href="/faq">FAQ</a>
+              <a className="navigation-mobile-item" href="/requst-form">REQUEST MOBIKE</a>
+              <a className="navigation-mobile-item">DOWNLOAD APP</a>
+            </div>
+          </div>
         </div>
       </div>
     ) 
